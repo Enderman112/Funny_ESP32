911620d2f987f7c8c256544d1c83599b5d31f468
xuyaoshi <xuyaoshix@gmail.com>
2026-05-24 21:57:54 +0800

    fix: use lv_font_montserrat_14 (default enabled)

--- main/main.cpp
+++ main/main.cpp
@@ -31,7 +31,7 @@ static void create_hello_world_ui(void)
 {
     lv_obj_t *label = lv_label_create(lv_scr_act());
     lv_label_set_text(label, "Hello World!");
-    lv_obj_set_style_text_font(label, &lv_font_montserrat_28, 0);
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_14, 0);
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 }
 
