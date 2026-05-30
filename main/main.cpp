8533effccdbead803b90bdc57a49e6e00fee854f
xuyaoshi <xuyaoshix@gmail.com>
2026-05-30 21:11:20 +0800

    fix: font static_bitmap, restore clock on other pages

--- main/main.cpp
+++ main/main.cpp
@@ -305,7 +305,13 @@ static void clock_task(void *arg)
     bool saying_fetched = false;
     while(1) {
         if (Lvgl_lock(-1)) {
-            update_hello_page();
+            if (info_page_active || deepseek_page_active || menu_visible) {
+                // 其他页面：显示小钟
+                update_clock();
+            } else {
+                // Hello World页面：显示大钟
+                update_hello_page();
+            }
             Lvgl_unlock();
         }
         
