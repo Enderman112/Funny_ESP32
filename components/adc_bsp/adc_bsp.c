5e91790251049a3ae6bcbddf09e21e56551b8621
xuyaoshi <xuyaoshix@gmail.com>
2026-06-04 20:51:20 +0800

    fix: charge icon 3.5V, battery full 4.2V

--- components/adc_bsp/adc_bsp.c
+++ components/adc_bsp/adc_bsp.c
@@ -61,9 +61,9 @@ uint8_t adc_bsp_get_battery_level(void)
     if (vol < 3.0f) {
         return 0;
     }
-    if (vol > 4.12f) {
+    if (vol > 4.2f) {
         return 100;
     }
-    float level = ((vol - 3.0f) / 1.12f) * 100;
+    float level = ((vol - 3.0f) / 1.2f) * 100;
     return (uint8_t)level;
 }
